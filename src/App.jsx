eac49b2567fc58e74009b1e0f41b1332716882ff
makam chiranjeevi <makamchiru123@gmail.com>
2025-08-12 22:29:07 +0530

    changed in the tiger

--- src/App.jsx
+++ src/App.jsx
@@ -31,7 +31,7 @@ function App() {
       <p>I want to learn devops</p> 
       <p>I started learing devops</p> 
       <p>my name is chiranjeevi mech.</p> 
-      <p> Tiger is national animal.</p>
+      <p> Tiger is the national animal and also wild animal.</p>
     </>
   )
 }
