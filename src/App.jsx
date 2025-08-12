c6dd1d2f57aa873b0054124360fd6f03ab72f5cd
makam chiranjeevi <makamchiru123@gmail.com>
2025-08-12 21:49:30 +0530

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -28,7 +28,8 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p> 
-      <p>I want to learn devops</p>
+      <p>I want to learn devops</p> 
+      <p>I started learing devops and cloud</p>
     </>
   )
 }
